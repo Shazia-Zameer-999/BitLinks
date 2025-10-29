f4678e592ac5944ddd646d04e7b021c644792b4b
Daten by SZ <datendiva7867@gmail.com>
2025-10-29 20:17:03 +0530

    project 2'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  
   ...compat.extends("next/core-web-vitals"),
+
+  {
+    rules: {
+      
+      "react/no-unescaped-entities": "off", 
+    }
+  },
+  
   {
     ignores: [
       "node_modules/**",
